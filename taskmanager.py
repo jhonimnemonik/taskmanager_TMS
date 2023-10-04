d3a886472518eb288aa1ad6d1324bf5cff5b4771
Vladislav Klechshin <Jannigma666@gmail.com>
2023-10-05 01:55:36 +0200

    fix error

--- taskmanager.py
+++ taskmanager.py
@@ -1,170 +1,110 @@
-import psutil
-import psutil, datetime
-import time
-_times=psutil.cpu_times()
+import psutil, datetime, time
 
-# Левый формат.
-def format_left(*argc):
-    print("|\t{:<90}|".format(*argc))
-    return
 
-# Заголовок
-def name(*argc):
-    print("|{:■^90}".format(*argc),end=" ⊟ ⊡ ⊠ |\n")
-    return
-
-def Info_get():
-    def format_right(*argc):
-        print("|\t{:>90}|".format(*argc))
-        return
-        # Имя пользователя
-    def user_get(): 
-        user=psutil.users()
-        for i in user:
-            format_right("User name :    "+i[0])
-            n=int(i[3])
-        time_format = time.strftime("%H:%M:%S", time.gmtime(n)) 
-        format_right("On system :"+time_format)
-
-    # Данные процессора  
-    def CPU_show():    
-        a=psutil.cpu_percent(interval=1)
-        while a!=None:
-            z=None
-            if a <=10.0:
-                z='[|.........]'
-                break
-            elif a<=20.0:
-                z='[||........]'
-                break
-            elif a<=30.0:
-                z='[|||.......]'
-                break
-            elif a<=40.0:
-                z='[||||......]'
-                break
-            elif a<=50.0:
-                z='[|||||.....]'
-                break   
-            elif a<=60.0:
-                z='[||||||....]'
-                break
-            elif a<=70.0:
-                z='[|||||||...]'
-                break 
-            elif a<=80.0:
-                z='[||||||||..]'
-                break 
-            elif a<=90.0:
-                z='[|||||||||.]'
-                break
-            elif a<=100.0:
-                z='[||||||||||]'
-                break                     
-        format_left("CPU        Load   : "+str(a)+"% "+ z)
-        format_left('CPU time   User   : '+ ("{:.1f}".format(_times[0])) + " Min.")
-        format_left('           System : '+ ("{:.1f}".format(_times[1]/60)) + " Min.")
-        format_left("") # space
-
-    # Оперативная память
-    def memory():
-        mem=psutil.virtual_memory()
-        percent=mem[2]
-        while percent!=None:
-            a=None
-            if percent <= 10:
-                a=('[|.........]')
-                break
-            elif percent <=20:
-                a=('[||........]')
-                break
-            elif percent <=30:
-                a=('[|||.......]')
-                break
-            elif percent <=40:
-                a=('[||||......]')
-                break
-            elif percent <=50:
-                a=('[|||||.....]')
-                break
-            elif percent <=60:
-                a=('[||||||....]')
-                break
-            elif percent <=70:
-                a=('[|||||||...]')
-                break
-            elif percent <=80:
-                a=('[||||||||..]')
-                break
-            elif percent <=90:
-                a=('[|||||||||.]')
-                break
-            elif percent <=100:
-                a=('[|||||||||!]')
-                break
-        format_left('Ram        Load   : ' + str(percent)+"% "+ a )
-        format_left('           Total  : '"{:.0f}".format(mem[0]/1000**2)+ "Mb.")
-        format_left('           Free   : '"{:.0f}".format(mem[4]/1000**2)+ "Mb.")
-        format_left("      ") # space
-
-    # Боот
-    def Boot_Time():
-        format_left('Boot time         : '+str(datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")))
-        format_left('Time since On     : '"{:.0f}".format(psutil.boot_time()/360)+' H.')
-        format_left("")
-        return
-    user_get()
-    CPU_show()
-    memory()
-    Boot_Time()
-
-# Список запущенных приложений
-def Process_list():
-    # Форматирование границы
-    def edge():
-        print("|{:-^97}|".format(''))
-        return
-    # Форматирование спсика
-    def format_this(*argc):
-        print("| {:<96}|".format(*argc))
-        return
-    #Количество Зап. прилож.
-    def runs():
-        num=0 
-        for p in psutil.process_iter():
-            num+=1
-        format_left('Runs applications : ' + str(num))
-        format_left("")
-
-    #Заголовок списка
-    def Name_List():
-        edge()
-        format_this((" №  | "+"{:<30}".format("Name")+ "|{:^10}".format("%")+ "|{:^10}".format("PID") + "|{:^10}| ".format("Status")+str("User")))
-        edge()
-    #Список прилож.
-        num=0    
-        for p in psutil.process_iter():
-            num+=1
-            a=p.as_dict(['pid'])
-            b=p.as_dict(['name'])
-            c=p.as_dict(['username'])
-            d=p.as_dict(['cpu_percent'])
-            e=p.as_dict(['status'])
-            if num <=9:
-                format_this(str(num)+".  | "+"{:<30}".format(b['name'])+ "|{:^10}".format(d['cpu_percent'])+ "|{:^10}".format(a['pid']) + "|{:^10}| ".format(e['status'])+str(c['username']))
-            elif num>=10 and num <=99:
-                format_this(str(num)+". | "+"{:<30}".format(b['name'])+ "|{:^10}".format(d['cpu_percent'])+ "|{:^10}".format(a['pid']) + "|{:^10}| ".format(e['status'])+str(c['username']))
-            elif num > 99:
-                format_this(str(num)+".| "+"{:<30}".format(b['name'])+ "|{:^10}".format(d['cpu_percent'])+ "|{:^10}".format(a['pid']) + "|{:^10}| ".format(e['status'])+str(c['username']))
-        edge()
-    runs()
-    Name_List()
-    return
-
-def Main():
-    name("Task Manager")
-    Info_get()
-    Process_list()
-    return
+def user_info_get():
+    users = psutil.users()
+    for user in users:
+        name = user[0]
+        time = user[3]
+        return name , time
     
-Main()
+
+def title_show(user_name, user_time , titl_name):
+    buton = "⊟ ⊡ ⊠ ╗"
+    print (f"╔{titl_name:═^104}" , f"{buton:<7}")
+    print ("║{:>90}{:<10}{:<10}║" .format ("" , "User name: " , user_name))
+    print ("║{:>90}{:<10}{:<10}║" .format ("" , "On system: " , time.strftime("%H:%M:%S" , time.gmtime(user_time))))
+
+
+def cpu_get():
+    _times = psutil.cpu_times()  
+    percent = psutil.cpu_percent(interval=0)
+    return _times[0] ,_times[1] , percent
+
+      
+def cpu_load_show(percent):
+    bar = "{:░<10}"
+    load = bar.format ("▓" * int(percent / 10))
+    return print ("║\t{:<10}{:<10}{:<10}{}{:<64}║".format("CPU" , "Load:" , str(percent) + " % " , load , ""))
+
+
+def cpu_timesup_show():
+    user_time = time.strftime("%H:%M:%S" , time.gmtime(cpu_get()[0]))
+    system_time = time.strftime("%H:%M:%S" , time.gmtime(cpu_get()[1]))
+    print ("║\t{:<10}{:<10}{:<10}{:<74}║".format("CPU time" , "User:" , str(user_time) , ""))
+    print ("║\t{:<10}{:<10}{:<10}{:<74}║".format("" , "System:" , str(system_time) , ""))
+    print ("║{:<111}║" .format(""))
+
+
+def memory_get():
+    mem_info = psutil.virtual_memory()
+    total = mem_info[0]
+    percent = mem_info[2]
+    free = mem_info[4]
+    return total , percent , free
+
+
+def memory_show(total , percent , free):
+    bar = "{:░<10}"
+    load = bar. format ("▓" * int(percent / 10))
+    print ("║\t{:<10}{:<10}{:<10}{}{:<64}║".format("Ram" , "Load:" , str(percent) + " % " , load , ""))
+    print ("║\t{:<10}{:<10}{:<10}{:<74}║".format("" , "Total:" , str(total//1000**2) + " Mb." , ""))
+    print ("║\t{:<10}{:<10}{:<10}{:<74}║".format("" , "Free:" , str(free//1000**2) + " Mb." , ""))
+    print ("║{:<111}║" .format(""))
+
+  
+def boot_time_show():
+    time_value = psutil.boot_time()
+    boot_time = datetime.datetime.fromtimestamp(time_value).strftime("%Y-%m-%d %H:%M:%S")
+    since_time = "{:.0f}".format(time_value/360)+' H.'
+    print ("║\t{:<15}{:<5}{:<30}{:<54}║" .format ("Boot time:" , "" , boot_time , ""))
+    print ("║\t{:<15}{:<5}{:<30}{:<54}║" .format ("Time since On:", ""  , since_time , ""))
+ 
+
+def number_apps_get():
+    num_app = 0 
+    for number in psutil.process_iter():
+        num_app += 1
+    return num_app
+ 
+
+def process_list_show(num_app):
+    print ("║\t{:<20}{:<10}{:<21}{:<53}║" .format ("Runs applications:" , num_app , "" , ""))
+    print ("║{:<111}║" .format(""))
+    print ("╠{:─^6}┬{:─^65}┬{:─^10}┬{:─^12}┬{:─^14}╣" .format ("─" , "─" , "─" , "─" , "─" , "─" ))
+    print ("║{:^6}│{:^65}│{:^10}│{:^12}│{:^14}║" .format ("№" , "Name" , "%" , "PID" , "Status", "User"))
+    print ("╠{:─^6}┼{:─^65}┼{:─^10}┼{:─^12}┼{:─^14}╣" .format ("─" , "─" , "─" , "─" , "─" , "─" ))
+
+    
+    def name_list(): 
+        n_app = 0
+
+        for list_string in psutil.process_iter():
+            n_app += 1   
+            pid = list_string.as_dict(['pid'])
+            name = list_string.as_dict(['name'])
+            user = list_string.as_dict(['username'])
+            perc = list_string.as_dict(['cpu_percent'])
+            stat = list_string.as_dict(['status'])
+            
+            print (
+                  "║{:^6}│{:<65}│{:^10}│{:^12}│{:^14}║" .format 
+                  (str(n_app) , name['name'] , perc['cpu_percent'] ,
+                  pid['pid'] , stat['status'] , user['username'])
+                  )
+    
+    name_list()
+    return print ("╚{:═^6}╧{:═^65}╧{:═^10}╧{:═^12}╧{:═^14}╝" .format ("═" , "═" , "═" , "═" , "═"))
+       
+
+def main():
+    title_show(user_info_get()[0] , user_info_get()[1] , "Task Manager")
+    cpu_load_show(cpu_get()[2])
+    cpu_timesup_show()
+    memory_show(memory_get()[0] , memory_get()[1] , memory_get()[2])
+    boot_time_show()
+    process_list_show(number_apps_get())
+
+    
+main()
+ 
